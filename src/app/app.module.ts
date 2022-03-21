263dee77ccca9f03dac77e84d719dd880fb76b0b
Tech Madi <zolasmandela4@gmail.com>
2022-03-21 20:37:38 +0300

    Edditing browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import { LandingComponent } from './modules/landing/landing.component';
 import { NotFoundPageComponent } from './modules/not-found-page/not-found-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, LandingComponent, NotFoundPageComponent],
